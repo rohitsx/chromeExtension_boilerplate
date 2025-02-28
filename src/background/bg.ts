11d87ec20007991f5ecfc5edf5ce4d567fae3917
rohit <rohiitrb@gmail.com>
2025-02-28 17:15:49 +0530

    first commit

--- src/background/bg.ts
+++ src/background/bg.ts
@@ -1,6 +1,12 @@
-chrome.webRequest.onCompleted.addListener(
-  function (details) {
-    console.log(details); // Capture the request details
-  },
-  { urls: ["<all_urls>"] },
-);
+chrome.runtime.onMessage.addListener((message) => {
+  const { url, action } = message;
+  const urlInstance = url.split(",");
+  const sendDetails = (details: any) => chrome.runtime.sendMessage(details);
+
+  if (action === "stopTracking") {
+    chrome.webRequest.onCompleted.removeListener(sendDetails);
+    return;
+  }
+
+  chrome.webRequest.onCompleted.addListener(sendDetails, { urls: urlInstance });
+});
