7ef6d76fd6504f37a9e3099b3cbbdbfbcc10e805
rohit <rohiitrb@gmail.com>
2025-02-28 12:52:23 +0530

    added background

---
+++ src/background/bg.ts
@@ -0,0 +1,1 @@
+console.log("alet working")
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
 import react from "@vitejs/plugin-react";
-import path from "path"
 import { crx, defineManifest } from "@crxjs/vite-plugin";
 import { defineConfig } from "vite";
 
 const manifest = defineManifest({
-  "manifest_version": 3,
-  "name": "changes_name",
-  "version": "1.0.0",
-  "action": { "default_popup": "index.html" },
+  manifest_version: 3,
+  name: "changes_name",
+  version: "1.0.0",
+  action: { default_popup: "index.html" },
+  background: {
+    service_worker: "src/background/bg.ts",
+    type: "module",
+  },
 });
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/shared"),
-      "@popup": path.resolve(__dirname, "./src/popup"),
-      "@background": path.resolve(__dirname, "./src/background"),
-      "@content": path.resolve(__dirname, "./src/content"),
-    },
-  },
-  plugins: [
-    react(),
-    crx({ manifest }),
-  ],
+  resolve: {},
+  plugins: [react(), crx({ manifest })],
 });
